95924542e72d49256ae7f62d8d38867fe684ca3e
Wayne_Mac <wayne65@gmail.com>
2020-12-01 22:12:30 +0800

    chore: Modify default ENV PORT to BOT_API_PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app.js');
 
-const port = process.env.PORT || 3333;
+const port = process.env.BOT_API_PORT || 3333;
 const server = http.createServer(app);
 
 server.listen(port);
